f06c3ace7153b444645d52ed4a0309bce78b8bab
WilsonArthur <harthurwilson2016@gmail.com>
2023-05-30 09:10:59 -0700

    _prinft.c

--- _printf.c
+++ _printf.c
@@ -1,57 +1,67 @@
+
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - produces output as written
- * @format: string passed in function
- * Return: return length of string
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-int i, counter;
-va_list list;
-va_start(list, format);
-counter = 0;
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-_print_chr(format[i]);
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
-else if (format[i + 1] == 's')
-{
-_print_str(va_arg(list, char *));
-}
-else if (format[i + 1] == '%')
-{
-_print_chr(format[i]);
-}
-counter++;
-}
-va_end(list);
-return (counter);
-}
+
 /**
- * _print_chr - prints out one character
- * @q: input character
- * Return: return 1
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-int _print_chr(char q)
+void print_buffer(char buffer[], int *buff_ind)
 {
-write(1, &q, 1);
-return (1);
-}
-/**
- * _print_str - prints out a string
- * @str: pointer to string being passed
- * Return: lenght of string
- */
-int _print_str(char *str)
-{
-int m, counter;
-counter = 0;
-for (m = 0; str[m] != '\0'; m++)
-{
-_print_chr(str[m]);
-counter++;
-}
-return (counter);
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
